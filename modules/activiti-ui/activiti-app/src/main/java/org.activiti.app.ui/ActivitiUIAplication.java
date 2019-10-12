7996533d8c4071e17f3af4a2ce5466f8e8feae37
19793 <1979309904@qq.com>
2019-10-13 00:20:28 +0800

    111

--- modules/activiti-ui/activiti-app/src/main/java/org.activiti.app.ui/ActivitiUIAplication.java
+++ modules/activiti-ui/activiti-app/src/main/java/org.activiti.app.ui/ActivitiUIAplication.java
@@ -1,18 +1,59 @@
 package org.activiti.app.ui;
 
+import org.activiti.app.conf.ApplicationConfiguration;
+import org.activiti.app.servlet.ApiDispatcherServletConfiguration;
+import org.activiti.app.servlet.AppDispatcherServletConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
+@Import(ApplicationConfiguration.class)
 public class ActivitiUIAplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        SpringApplication.run(ActivitiUIAplication.class,args);
+        SpringApplication.run(ActivitiUIAplication.class, args);
     }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(ActivitiUIAplication.class);
+    }
+
+    @Bean
+    public ServletRegistrationBean apiDispatcher() {
+        DispatcherServlet dispatcherServlet = new DispatcherServlet();
+
+        dispatcherServlet.setContextClass(AnnotationConfigWebApplicationContext.class);
+        dispatcherServlet.setContextConfigLocation(ApiDispatcherServletConfiguration.class.getName());
+
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
+        servletRegistrationBean.setServlet(dispatcherServlet);
+        servletRegistrationBean.addUrlMappings("/api/*");
+        servletRegistrationBean.setLoadOnStartup(1);
+        servletRegistrationBean.setEnabled(true);
+        servletRegistrationBean.setName("api");
+        return servletRegistrationBean;
+    }
+
+    @Bean
+    public ServletRegistrationBean appDispatcher() {
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet();
+        dispatcherServlet.setContextClass(AnnotationConfigWebApplicationContext.class);
+        dispatcherServlet.setContextConfigLocation(AppDispatcherServletConfiguration.class.getName());
+
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
+        servletRegistrationBean.setServlet(dispatcherServlet);
+        servletRegistrationBean.addUrlMappings("/app/*");
+        servletRegistrationBean.setLoadOnStartup(1);
+        servletRegistrationBean.setEnabled(true);
+        servletRegistrationBean.setName("app");
+        return servletRegistrationBean;
     }
 }
